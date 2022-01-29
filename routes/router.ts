2c172f1d02c8e13d7afccad31cbeb53385515c09
Alejandro González Reyes <alexglzrys@gmail.com>
2022-01-29 17:01:43 -0600

    enviar mensajes privados a través del socket

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import Server from '../clases/server';
 
 const router: Router = Router();
 
@@ -21,6 +22,12 @@ router.post('/mensaje', (req: Request, res: Response) => {
 router.post('/mensajes/:id', (req: Request, res: Response) => {
     const { cuerpo, de } = req.body;
     const id: string = req.params.id
+
+    // Enviar mensaje privado al usuario con id especificado (ID que el WS le asigna al conectarse)
+    const server = Server.instance;
+    const payload = {de, cuerpo};
+    // En este caso la sala corresponde a su ID asignado por el WS
+    server.io.in(id).emit('mensaje-privado', payload);
 
     res.json({
         ok: true,
